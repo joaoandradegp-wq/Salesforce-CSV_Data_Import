68e603ec7587d23084a5f42af271e3ba71a04113
João Andrade <joao.andrade.gp@gmail.com>
2026-06-10 20:13:34 -0300

    Update and rename pyinstallerEXE_dataimport_multi_PF.bat to pyinstallerEXE.bat

---
+++ pyinstallerEXE.bat
@@ -0,0 +1,2 @@
+pyinstaller --onefile --noconsole --icon=PF.ico dataimport_multi_PF.py
+pyinstaller --onefile --noconsole --icon=PJ.ico dataimport_PJ.py
--- pyinstallerEXE_dataimport_multi_PF.bat
+++
@@ -1,1 +0,0 @@
-pyinstaller --onefile --noconsole --icon=dataimport_multi_PF.ico dataimport_multi_PF.py
