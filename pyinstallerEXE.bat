1d548bf6bea8c13e212d5d882260d156c4c00ad5
João Andrade <joao.andrade.gp@gmail.com>
2026-08-18 20:27:30 -0300

    Nomes do PY e novo .BAT

--- pyinstallerEXE.bat
+++ pyinstallerEXE.bat
@@ -1,2 +1,4 @@
 pyinstaller --onefile --noconsole --icon=PF.ico SF-DataImport_PF-Multi.py
 pyinstaller --onefile --noconsole --icon=PJ.ico SF-DataImport_PJ.py
+pyinstaller --onefile --noconsole --icon=PJ.ico SF-SOQL_Generator.py
+pyinstaller --onefile --noconsole --icon=PJ.ico SF-Update_Contract_Database_PF.py
