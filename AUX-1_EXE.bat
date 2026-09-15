39d5d8b907eb5309ffbd852c446ee3ae47df34ef
João Andrade <joao.andrade.gp@gmail.com>
2026-09-14 22:55:34 -0300

    Update batch file to use AUX scripts

--- AUX-1_EXE.bat
+++
@@ -1,2 +0,0 @@
-pyinstaller --onefile --noconsole --icon=AUX-1.ico SF-SOQL_Generator.py
-pyinstaller --onefile --noconsole --icon=AUX-1.ico SF-Update_Contract_Database_PF.py
---
+++ AUX_EXE.bat
@@ -0,0 +1,2 @@
+pyinstaller --onefile --noconsole --icon=AUX-1.ico AUX-SOQL_Generator.py
+pyinstaller --onefile --noconsole --icon=AUX-1.ico AUX-Update_Contract_Database_PF.py
